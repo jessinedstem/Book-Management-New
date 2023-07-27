d84a39ca8544c6d588063dd076405c11b5ab3918
jessin <jessin.rajan@edstem.com>
2023-07-27 12:07:39 +0530

    variation

--- src/main/java/com/example/restapi/bookmanagementnew/dto/BookDto.java
+++ src/main/java/com/example/restapi/bookmanagementnew/dto/BookDto.java
@@ -2,11 +2,11 @@ package com.example.restapi.bookmanagementnew.dto;
 
 import com.example.restapi.bookmanagementnew.validation.ValidAuthor;
 import com.example.restapi.bookmanagementnew.validation.ValidIsbn;
+//import com.example.restapi.bookmanagementnew.validation.ValidPublicationDate;
 import com.example.restapi.bookmanagementnew.validation.ValidTitle;
 import java.time.LocalDate;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
-
 @Getter
 @Setter
 @Builder
